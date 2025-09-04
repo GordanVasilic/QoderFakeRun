d09935ae4e3ed6d685b47905e8389c9fad9db02a
Gordan Vasilic <vasilic.gordan@live.com>
2025-09-04 11:41:15 +0200

    Add Vercel Speed Insights integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from '@/components/NavBar'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -37,6 +38,7 @@ export default function RootLayout({
           </main>
         </div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
